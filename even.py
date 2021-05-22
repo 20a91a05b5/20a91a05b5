b54c18f3daf4f6a6c429767153868d7a9f6e163f
20a91a05b5 <84494982+20a91a05b5@users.noreply.github.com>
2021-05-22 13:48:04 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,12 @@
+#python to check given number is even or not
+num=int(input('enter number:'))
+if(num%2)==0:
+print(' given number is an even number')
+else:
+print('given number is an odd number')
+
+
+
+'''output:
+enter number:2
+given number is an even number
